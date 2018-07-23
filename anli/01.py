22fef65f25e8db263c0a35d22d2c0df2ddb12a2a
huangweiming <1126107283@qq.com>
2018-07-23 13:57:22 +0800

    测试调试的例子

---
+++ anli/01.py
@@ -0,0 +1,9 @@
+def SayHello(name):
+    print("I want to sya hello with you ,{0}".format(name))
+    print("hello,{0}".format(name))
+    print("Done..............")
+if __name__ == "__main__":
+    print("***"*10)
+    name = input("Please input your name :")
+    print(SayHello(name=name))
+    print1("@@@"*10)
